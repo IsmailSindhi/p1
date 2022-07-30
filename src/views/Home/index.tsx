ea783ff98b7c9abc3750000fe77b6e0ea83cd3bb
unknown <IsmailSindhi>
2022-07-30 10:34:08 -0700

    first commit

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,8 +1,9 @@
+import Navbar from '@components/Navbar';
 import HeroSection from './components/HeroSection';
-
 const Home = () => {
   return (
     <>
+      <Navbar/>
       <HeroSection />
     </>
   );
